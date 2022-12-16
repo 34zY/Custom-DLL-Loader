2e0aedf7f9e93233c893963bb3208e490a3743c1
/34ZY <55347142+34zY@users.noreply.github.com>
2022-12-16 12:01:32 +0100

    Add files via upload

---
+++ DLL-LOADER.cpp
@@ -0,0 +1,26 @@
+//g++ DLL-LOADER.cpp -o DLL-LOADER.exe
+
+#include <windows.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+typedef void (*Myfunction)(void);
+
+int main(int argc, char const *argv[])
+{
+	HINSTANCE hinst;
+	Myfunction Hello;
+	hinst = LoadLibrary("zbay.dll");
+	if (!hinst)
+	{
+		MessageBox(0, "DLL File not found !", "Error", MB_ICONERROR);
+		exit(0);
+	}
+	Hello = (Myfunction)GetProcAddress(hinst, "Zbay");
+	Hello();
+	FreeLibrary(hinst);
+	printf("OK\n");
+	exit(0);
+
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+//gcc -c main.c **MAKE object file**
+//gcc -shared -o main.dll main.o **MAKE DLL**
+
+#include <stdio.h>
+#include <windows.h>
+
+int Zbay()
+{
+	printf("Zbayyyyyy ....");
+	MessageBoxW(NULL, L"AAAAAAAAAH!!!", L"YYYYYYYYYYYYYYYYYESS", MB_ICONEXCLAMATION);
+	return 0;
+}
